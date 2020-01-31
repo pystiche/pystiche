7c3ef91f1fadb8070811d624e6b544cf401a20e5
Philip Meier <github.pmeier@posteo.de>
2020-01-31 11:35:37 +0100

    * moved layer_weights functionality from MultiLayerEncodingOperator to ContainerOperator * added region_weights option to MultiRegionOperator

--- pystiche/ops/container.py
+++ pystiche/ops/container.py
@@ -1,7 +1,6 @@
 from typing import Union, Sequence, Callable
 from collections import OrderedDict
 import torch
-from pystiche.misc import build_obj_str
 from pystiche.enc import Encoder, MultiLayerEncoder
 from .op import Operator, EncodingOperator, ComparisonOperator
 from .guidance import Guidance, ComparisonGuidance
@@ -10,67 +9,57 @@ __all__ = ["ContainerOperator", "MultiLayerEncodingOperator", "MultiRegionOperat
 
 
 class ContainerOperator(Operator):
-    def __init__(self, *args: Operator, score_weight=1e0) -> None:
+    def __init__(
+        self,
+        names: Sequence[str],
+        get_op: Callable[[str, float], Operator],
+        op_weights: Union[str, Sequence[float]] = "sum",
+        score_weight=1e0,
+    ) -> None:
         super().__init__(score_weight=score_weight)
-        if len(args) == 1 and isinstance(args[0], OrderedDict):
-            for key, module in args[0].items():
-                self.add_module(key, module)
+        op_weights = self._parse_op_weights(op_weights, len(names))
+        for name, weight in zip(names, op_weights):
+            op = get_op(name, weight)
+            self.add_module(name, op)
+
+    @staticmethod
+    def _parse_op_weights(op_weights, num_ops):
+        if isinstance(op_weights, str):
+            if op_weights == "sum":
+                return [1.0] * num_ops
+            elif op_weights == "mean":
+                return [1.0 / num_ops] * num_ops
+
+            raise ValueError
         else:
-            for idx, module in enumerate(args):
-                self.add_module(str(idx), module)
+            if len(op_weights) == num_ops:
+                return op_weights
+
+            raise ValueError
 
     def process_input_image(self, input_image: torch.Tensor) -> torch.Tensor:
         return self.score_weight * sum([op(input_image) for op in self.children()])
 
-    def __getitem__(self, item: Union[str, int]):
-        if isinstance(item, str):
-            return self._get_children_by_name(item)
-        elif isinstance(item, int):
-            return self._get_children_by_index(item)
-        else:
-            raise TypeError
-
-    def _get_children_by_name(self, name: str) -> Operator:
-        children = dict(self.named_children())
-        return children[name]
-
-    def _get_children_by_index(self, idx: int) -> Operator:
-        return tuple(self.children())[idx]
+    def __getitem__(self, name):
+        return self._modules[name]
 
 
 class MultiLayerEncodingOperator(ContainerOperator):
     def __init__(
         self,
+        layers: Sequence[str],
         get_encoding_op: Callable[[Encoder, float], EncodingOperator],
         multi_layer_encoder: MultiLayerEncoder,
-        layers: Sequence[str],
         layer_weights: Union[str, Sequence[float]] = "mean",
-        score_weight: float = 1.0,
+        score_weight: float = 1e0,
     ):
-        layer_weights = self._parse_layer_weights(layer_weights, len(layers))
-        ops = OrderedDict(
-            [
-                (layer, get_encoding_op(multi_layer_encoder[layer], layer_weight))
-                for layer, layer_weight in zip(layers, layer_weights)
-            ]
+        def get_op(layer, layer_weight):
+            encoder = multi_layer_encoder[layer]
+            return get_encoding_op(encoder, layer_weight)
+
+        super().__init__(
+            layers, get_op, op_weights=layer_weights, score_weight=score_weight
         )
-
-        super().__init__(ops, score_weight=score_weight)
-
-    @staticmethod
-    def _parse_layer_weights(layer_weights, num_layers):
-        if isinstance(layer_weights, str):
-            if layer_weights == "mean":
-                return [1.0 / num_layers] * num_layers
-            elif layer_weights == "sum":
-                return [1.0] * num_layers
-
-            raise ValueError
-        else:
-            if len(layer_weights) == num_layers:
-                return layer_weights
-
-            raise ValueError
 
     def set_target_guide(self, guide: torch.Tensor):
         for op in self.children():
@@ -114,10 +103,16 @@ class MultiLayerEncodingOperator(ContainerOperator):
 
 
 class MultiRegionOperator(ContainerOperator):
-    def __init__(self, regions, get_op, score_weight=1e0):
-        # FIXME: make a layer weights equivalent
-        ops = OrderedDict([(region, get_op()) for region in regions])
-        super().__init__(ops, score_weight=score_weight)
+    def __init__(
+        self,
+        regions: Sequence[str],
+        get_op: Callable[[Encoder, float], Operator],
+        region_weights: Union[str, Sequence[float]] = "sum",
+        score_weight: float = 1e0,
+    ):
+        super().__init__(
+            regions, get_op, op_weights=region_weights, score_weight=score_weight
+        )
 
     def set_target_guide(self, region, guide):
         self[region].set_target_guide(guide)
