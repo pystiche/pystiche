d0d9e50f4255f64ad6d4822a65e0d57730e9a964
Philip Meier <philip.meier@th-owl.de>
2020-02-07 22:11:42 +0100

    Add container modules (#56)

--- pystiche/core/_modules.py
+++ pystiche/core/_modules.py
@@ -1,13 +1,39 @@
 from abc import abstractmethod
-from typing import Any, Dict
+from typing import Any, Union, Dict
+from collections import OrderedDict
+import torch
 from torch import nn
 from ._base import Object
+
+__all__ = ["Module", "ContainerModule", "SequentialModule"]
 
 
 class Module(nn.Module, Object):
     @abstractmethod
-    def forward(self, *args: Any, **kwargs: Dict[str, Any]) -> Any:
+    def forward(self, *args: Any, **kwargs: Any) -> Any:
         pass
 
     def extra_repr(self) -> str:
         return ", ".join([f"{key}={value}" for key, value in self.properties().items()])
+
+
+class ContainerModule(Module):
+    def __init__(self, *args: Union[nn.Module, Dict[str, nn.Module]]) -> None:
+        super().__init__()
+        if len(args) == 1 and isinstance(args[0], OrderedDict):
+            for key, module in args[0].items():
+                self.add_module(key, module)
+        else:
+            for idx, module in enumerate(args):
+                self.add_module(str(idx), module)
+
+    @abstractmethod
+    def forward(self, *args: Any, **kwargs: Any) -> Any:
+        pass
+
+
+class SequentialModule(ContainerModule):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for module in self.children():
+            x = module(x)
+        return x
