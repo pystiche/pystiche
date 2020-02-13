fix normalization transforms

pystiche/image/transforms/functional/misc.py
@@ -1,7 +1,6 @@
-from typing import Optional
+from typing import Optional, Sequence, Tuple
 import torch
-from pystiche.image.utils import force_batched_image
-from pystiche.typing import Numeric
+from pystiche.image.utils import extract_num_channels, force_batched_image
 
 __all__ = [
     "normalize",
@@ -28,12 +27,32 @@ def reverse_channel_order(x: torch.Tensor) -> torch.Tensor:
 
 
 @force_batched_image
-def normalize(x: torch.Tensor, mean: Numeric, std: Numeric) -> torch.Tensor:
+def _channel_stats_to_tensor(
+    image: torch.Tensor, mean: Sequence[float], std: Sequence[float]
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    num_channels = extract_num_channels(image)
+
+    def to_tensor(seq: Sequence[float]) -> torch.Tensor:
+        if len(seq) != num_channels:
+            raise RuntimeError
+        return torch.tensor(seq, device=image.device).view(1, -1, 1, 1)
+
+    mean = to_tensor(mean)
+    std = to_tensor(std)
+    return mean, std
+
+
+def normalize(
+    x: torch.Tensor, mean: Sequence[float], std: Sequence[float]
+) -> torch.Tensor:
+    mean, std = _channel_stats_to_tensor(x, mean, std)
     return (x - mean) / std
 
 
-@force_batched_image
-def denormalize(x: torch.Tensor, mean: Numeric, std: Numeric) -> torch.Tensor:
+def denormalize(
+    x: torch.Tensor, mean: Sequence[float], std: Sequence[float]
+) -> torch.Tensor:
+    mean, std = _channel_stats_to_tensor(x, mean, std)
     return x * std + mean
 
 

pystiche/image/transforms/misc.py
@@ -1,7 +1,6 @@
 from typing import Any, Sequence, Dict
 import torch
 from pystiche.misc import to_engtuplestr
-from pystiche.typing import Numeric
 from .core import Transform
 from . import functional as F
 
@@ -30,13 +29,10 @@ class ReverseChannelOrder(Transform):
 
 
 class Normalize(Transform):
-    def __init__(self, mean: Sequence[Numeric], std: Sequence[Numeric]) -> None:
+    def __init__(self, mean: Sequence[float], std: Sequence[float]) -> None:
         super().__init__()
         self.mean = mean
         self.std = std
-
-        self.register_buffer("_mean", torch.tensor(mean).view(1, -1, 1, 1))
-        self.register_buffer("_std", torch.tensor(std).view(1, -1, 1, 1))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return F.normalize(x, self._mean, self._std)
@@ -49,5 +45,16 @@ class Normalize(Transform):
 
 
 class Denormalize(Normalize):
+    def __init__(self, mean: Sequence[float], std: Sequence[float]) -> None:
+        super().__init__()
+        self.mean = mean
+        self.std = std
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return F.denormalize(x, self._mean, self._std)
+
+    def _properties(self) -> Dict[str, Any]:
+        dct = super()._properties()
+        dct["mean"] = to_engtuplestr(self.mean)
+        dct["std"] = to_engtuplestr(self.std)
+        return dct