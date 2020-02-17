049ae9d2ee20a8d702d24b31dedfed2ff05df655
Philip Meier <github.pmeier@posteo.de>
2020-02-17 20:57:52 +0100

    add tests

--- pystiche/image/transforms/crop.py
+++ pystiche/image/transforms/crop.py
@@ -1,43 +1,15 @@
 from typing import Any, Union, Tuple, Dict
 import torch
-from pystiche.misc import verify_str_arg
 from .core import Transform
 from . import functional as F
 
 __all__ = [
-    "Crop",
     "CenterCrop",
     "TopLeftCrop",
     "BottomLeftCrop",
     "TopRightCrop",
     "BottomRightCrop",
 ]
-
-
-class Crop(Transform):
-    def __init__(
-        self,
-        origin: Tuple[int, int],
-        size: Union[Tuple[int, int], int],
-        vert_anchor: str = "top",
-        horz_anchor: str = "left",
-    ):
-        super().__init__()
-        self.origin = origin
-        self.size = size
-        self.vert_anchor = verify_str_arg(vert_anchor, "vert_anchor", ("top", "bottom"))
-        self.horz_anchor = verify_str_arg(horz_anchor, "horz_anchor", ("left", "right"))
-
-    def forward(self, image: torch.Tensor) -> torch.Tensor:
-        return F.crop(image, self.origin, self.size)
-
-    def _properties(self) -> Dict[str, Any]:
-        dct = super()._properties()
-        dct["origin"] = self.origin
-        dct["size"] = self.size
-        dct["vert_anchor"] = self.vert_anchor
-        dct["horz_anchor"] = self.horz_anchor
-        return dct
 
 
 class TopLeftCrop(Transform):
@@ -60,7 +32,7 @@ class BottomLeftCrop(Transform):
         self.size = size
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
-        return F.top_left_crop(image, self.size)
+        return F.bottom_left_crop(image, self.size)
 
     def _properties(self) -> Dict[str, Any]:
         dct = super()._properties()
@@ -74,7 +46,7 @@ class TopRightCrop(Transform):
         self.size = size
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
-        return F.top_left_crop(image, self.size)
+        return F.top_right_crop(image, self.size)
 
     def _properties(self) -> Dict[str, Any]:
         dct = super()._properties()
@@ -88,7 +60,7 @@ class BottomRightCrop(Transform):
         self.size = size
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
-        return F.top_left_crop(image, self.size)
+        return F.bottom_right_crop(image, self.size)
 
     def _properties(self) -> Dict[str, Any]:
         dct = super()._properties()
