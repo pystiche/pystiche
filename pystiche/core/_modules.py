pystiche/pystiche

commit 2ecbc8312004ef61f29643c32f15a45828afa182

Refactor container modules (#66)

pystiche/core/_modules.py
@@ -1,14 +1,39 @@
 from abc import abstractmethod
-from typing import Any, Union, Dict
+from typing import Any, Optional, Sequence, Dict
 from collections import OrderedDict
 import torch
 from torch import nn
 from ._base import Object
 
-__all__ = ["Module", "ContainerModule", "SequentialModule"]
+__all__ = ["Module", "SequentialModule"]
 
 
 class Module(nn.Module, Object):
+    def __init__(
+        self,
+        named_children: Optional[Dict[str, nn.Module]] = None,
+        indexed_children: Optional[Sequence[nn.Module]] = None,
+    ):
+        if named_children is not None and indexed_children is not None:
+            msg = (
+                "named_children and indexed_children "
+                "are mutually exclusive parameters."
+            )
+            raise RuntimeError(msg)
+        elif named_children is not None:
+            self.add_named_modules(named_children)
+        elif indexed_children is not None:
+            self.add_indexed_modules(indexed_children)
+
+    def add_named_modules(self, modules: Dict[str, nn.Module]) -> None:
+        for name, module in modules.items():
+            self.add_module(name, module)
+
+    def add_indexed_modules(self, modules: Sequence[nn.Module]) -> None:
+        self.add_named_modules(
+            OrderedDict([(str(idx), module) for idx, module in enumerate(modules)])
+        )
+
     @abstractmethod
     def forward(self, *args: Any, **kwargs: Any) -> Any:
         pass
@@ -17,22 +42,7 @@ class Module(nn.Module, Object):
         return ", ".join([f"{key}={value}" for key, value in self.properties().items()])
 
 
-class ContainerModule(Module):
-    def __init__(self, *args: Union[nn.Module, Dict[str, nn.Module]]) -> None:
-        super().__init__()
-        if len(args) == 1 and isinstance(args[0], OrderedDict):
-            for key, module in args[0].items():
-                self.add_module(key, module)
-        else:
-            for idx, module in enumerate(args):
-                self.add_module(str(idx), module)
-
-    @abstractmethod
-    def forward(self, *args: Any, **kwargs: Any) -> Any:
-        pass
-
-
-class SequentialModule(ContainerModule):
+class SequentialModule(Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for module in self.children():
             x = module(x)

pystiche/enc/encoder.py
@@ -21,10 +21,8 @@ class Encoder(pystiche.Module):
 
 
 class SequentialEncoder(Encoder):
-    def __init__(self, modules: Dict[str, nn.Module]) -> None:
-        super().__init__()
-        for name, module in modules.items():
-            self.add_module(name, module)
+    def __init__(self, modules: Sequence[nn.Module]) -> None:
+        super().__init__(indexed_children=modules)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for module in self.children():
@@ -60,9 +58,9 @@ class SingleLayerEncoder(Encoder):
         )
 
 
-class MultiLayerEncoder(pystiche.ContainerModule):
-    def __init__(self, *args: Union[nn.Module, Dict[str, nn.Module]]) -> None:
-        super().__init__(*args)
+class MultiLayerEncoder(pystiche.Module):
+    def __init__(self, modules: Dict[str, nn.Module]) -> None:
+        super().__init__(named_children=modules)
         self._registered_layers = set()
         self._storage = {}
 

pystiche/ops/container.py
@@ -6,14 +6,13 @@ from pystiche.enc import Encoder, MultiLayerEncoder
 from .op import Operator, EncodingOperator, ComparisonOperator
 from .guidance import Guidance, ComparisonGuidance
 
-__all__ = ["Container", "MultiLayerEncodingOperator", "MultiRegionOperator"]
+__all__ = ["OperatorContainer", "MultiLayerEncodingOperator", "MultiRegionOperator"]
 
 
-class Container(Operator):
+class OperatorContainer(Operator):
     def __init__(self, named_ops: Dict[str, Operator], score_weight=1e0):
         super().__init__(score_weight=score_weight)
-        for name, op in named_ops.items():
-            self.add_module(name, op)
+        self.add_named_modules(named_ops)
 
     def process_input_image(self, input_image: torch.Tensor) -> pystiche.LossDict:
         return pystiche.LossDict(
@@ -24,7 +23,7 @@ class Container(Operator):
         return self._modules[name]
 
 
-class SameOperatorContainer(Container):
+class SameOperatorContainer(OperatorContainer):
     def __init__(
         self,
         names: Sequence[str],

commit 85ce547e5fa6ef9162a21bed88800e12adf99449

fix bugs introduced by 2ecbc8312004ef61f29643c32f15a45828afa182

pystiche/core/_modules.py
@@ -14,6 +14,7 @@ class Module(nn.Module, Object):
         named_children: Optional[Dict[str, nn.Module]] = None,
         indexed_children: Optional[Sequence[nn.Module]] = None,
     ):
+        super().__init__()
         if named_children is not None and indexed_children is not None:
             msg = (
                 "named_children and indexed_children "
@@ -43,6 +44,9 @@ class Module(nn.Module, Object):
 
 
 class SequentialModule(Module):
+    def __init__(self, *modules: nn.Module):
+        super().__init__(indexed_children=modules)
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for module in self.children():
             x = module(x)

pystiche/image/transforms/core.py
@@ -1,6 +1,7 @@
 from typing import Union, Iterable
 from abc import abstractmethod
 import itertools
+import torch
 import pystiche
 
 __all__ = ["Transform", "ComposedTransform"]
@@ -17,7 +18,15 @@ class Transform(pystiche.Module):
         return compose_transforms(self, other)
 
 
-class ComposedTransform(pystiche.SequentialModule):
+class ComposedTransform(Transform):
+    def __init__(self, *transforms: Transform):
+        super().__init__(indexed_children=transforms)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for module in self.children():
+            x = module(x)
+        return x
+
     def __add__(
         self, other: Union["Transform", "ComposedTransform"]
     ) -> "ComposedTransform":