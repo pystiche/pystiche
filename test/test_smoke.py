9e9904715d826b11a9ebb786ed2aba9c6ca3bf4e
Philip Meier <philip.meier@th-owl.de>
2020-03-12 14:11:20 +0100

    add smoke test for imports (#97)

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -1,8 +1,16 @@
 import unittest
+from os import path
+from setuptools import find_packages
+from importlib import import_module
 import pystiche
 
 
 class Tester(unittest.TestCase):
+    def test_import(self):
+        here = path.abspath(path.dirname(__file__))
+        for module in find_packages(path.join(here, "..", "pystiche")):
+            import_module(f"pystiche.{module}")
+
     def test_about(self):
         for attr in (
             "name",
