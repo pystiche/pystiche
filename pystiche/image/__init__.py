5a4867d0eb51c3c92354becb4e1bce260af80528
Philip Meier <github.pmeier@posteo.de>
2020-04-02 16:55:41 +0200

    add deprecation warning

--- pystiche/image/__init__.py
+++ pystiche/image/__init__.py
@@ -1,2 +1,5 @@
 from .io import *
 from .utils import *
+
+# Remove this together with .processing.py
+from .transforms.processing import *
---
+++ pystiche/image/processing.py
@@ -0,0 +1,10 @@
+from pystiche.misc import warn_deprecation
+from .transforms.processing import *
+
+# If removed, also remove import in pystiche.image.__init__.py
+warn_deprecation(
+    "module",
+    "pystiche.image.processing",
+    "0.4",
+    info="It was moved to pystiche.image.transforms.processing",
+)
