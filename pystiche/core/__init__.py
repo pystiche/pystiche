5da0251e9618e8cfcb00327d895a6bb213a99182
Philip Meier <github.pmeier@posteo.de>
2020-04-15 11:36:36 +0200

    explicitly skip files where import order is crucial

--- pystiche/core/__init__.py
+++ pystiche/core/__init__.py
@@ -1,6 +1,6 @@
+from ._meta import *
 from ._base import *
 from ._home import *
 from ._math import *
-from ._meta import *
 from ._modules import *
 from ._utils import *
--- pystiche/image/__init__.py
+++ pystiche/image/__init__.py
@@ -1,3 +1,3 @@
+from .utils import *
 from .io import *
 from .transforms.processing import *
-from .utils import *
