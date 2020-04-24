a27e856f0c9007bd032d55d780db0e2f052e008c
Philip Meier <github.pmeier@posteo.de>
2020-04-24 11:10:34 +0200

    enable bibtex referencing in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,6 +72,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.coverage",
     "sphinx_autodoc_typehints",
+    "sphinxcontrib.bibtex",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,4 +10,5 @@ Welcome to pystiche's documentation!
   :maxdepth: 2
 
   Getting Started <getting_started>
-  Tutorials <tutorials>
+  Examples <auto_examples/index.rst>
+  Literature <literature>
