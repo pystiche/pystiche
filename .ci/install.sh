8a13b4a766164965bd29f1ec2f6f47652f8e379f
Philip Meier <github.pmeier@posteo.de>
2020-05-04 17:00:09 +0200

    upgrade pip before install

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+pip3 install --upgrade pip
+
 REQUIREMENTS_FILE=ci_torch_cpu_requirements.txt
 python3 gen_torch_cpu_requirements.py --file $REQUIREMENTS_FILE
 pip3 install -r $REQUIREMENTS_FILE
