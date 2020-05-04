2a7f135b1535935e612d2deab9bd942e83ffd7b1
Philip Meier <github.pmeier@posteo.de>
2020-05-04 17:07:15 +0200

    add onerror function to handle permission errors on windows

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,7 @@
 import contextlib
+import os
 import shutil
+import stat
 import tempfile
 from os import path
 
@@ -136,13 +138,34 @@ class PysticheTestCase(pyimagetest.ImageTestCase):
             return dill.load(fh)
 
 
+# Adapted from
+# https://pypi.org/project/pathutils/
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    if not os.access(path, os.W_OK):
+        # Is the error an access error ?
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
+
+
 @contextlib.contextmanager
 def get_tmp_dir(**mkdtemp_kwargs):
     tmp_dir = tempfile.mkdtemp(**mkdtemp_kwargs)
     try:
         yield tmp_dir
     finally:
-        shutil.rmtree(tmp_dir)
+        shutil.rmtree(tmp_dir, onerror=onerror)
 
 
 skip_if_cuda_not_available = pytest.mark.skipif(
