807f1eea89cf37bac77f513100916411c358eb60
Philip Meier <github.pmeier@posteo.de>
2020-05-04 15:05:06 +0200

    run tests on multiple oss and python versions

---
+++ .ci/after_success.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+coverage report
+codecov -X gcov
---
+++ .ci/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+REQUIREMENTS_FILE=ci_torch_cpu_requirements.txt
+python3 gen_torch_cpu_requirements.py --file $REQUIREMENTS_FILE
+pip3 install -r $REQUIREMENTS_FILE
+
+pip3 install --upgrade .[test] coverage codecov
+pip3 list
