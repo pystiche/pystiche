68bfc82ded2425a1ad74026787acbef9a5e9c5e5
Philip Meier <github.pmeier@posteo.de>
2020-05-25 09:01:20 +0200

    remove meta types since they serve no purpose (#258)

--- pystiche/typing.py
+++ pystiche/typing.py
@@ -1,24 +1,13 @@
 import warnings
 
-from pystiche.meta import (
-    ConvModule,
-    ConvModuleMeta,
-    PoolModule,
-    PoolModuleMeta,
-    TensorMeta,
-    is_conv_module,
-    is_pool_module,
-)
+from pystiche.meta import ConvModule, PoolModule, is_conv_module, is_pool_module
 from pystiche.misc import build_deprecation_message
 
 __all__ = [
-    "TensorMeta",
     "ConvModule",
     "is_conv_module",
-    "ConvModuleMeta",
     "PoolModule",
     "is_pool_module",
-    "PoolModuleMeta",
 ]
 
 
