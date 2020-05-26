55b079c619385924fd2a9905e57a21de1eeeb814
Philip Meier <github.pmeier@posteo.de>
2020-05-26 15:23:36 +0200

    add typehints for pystiche.data

--- pystiche/data/sampler.py
+++ pystiche/data/sampler.py
@@ -10,13 +10,13 @@ __all__ = [
 
 
 class InfiniteCycleBatchSampler(Sampler):
-    def __init__(self, data_source: Sized, batch_size: int = 1):
+    def __init__(self, data_source: Sized, batch_size: int = 1) -> None:
         super().__init__(data_source)
         self.data_source = data_source
         self.batch_size = batch_size
 
     def __iter__(self) -> Iterator[Tuple[int, ...]]:
-        def nextn(iterator, n):
+        def nextn(iterator: Iterator[int], n: int) -> Iterator[int]:
             for _ in range(n):
                 yield next(iterator)
 
@@ -26,7 +26,9 @@ class InfiniteCycleBatchSampler(Sampler):
 
 
 class FiniteCycleBatchSampler(InfiniteCycleBatchSampler):
-    def __init__(self, data_source: Sized, num_batches: int, batch_size: int = 1):
+    def __init__(
+        self, data_source: Sized, num_batches: int, batch_size: int = 1
+    ) -> None:
         super().__init__(data_source, batch_size=batch_size)
         self.num_batches = num_batches
 
