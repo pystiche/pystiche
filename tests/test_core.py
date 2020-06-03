9bd1be2c02695785e5dce9a272f93f06f1142b3d
Philip Meier <github.pmeier@posteo.de>
2020-06-03 10:12:06 +0200

    rename possqrt to nonnegsqrt

--- tests/test_core.py
+++ tests/test_core.py
@@ -345,6 +345,29 @@ class TestMath(PysticheTestCase):
             actual = x.grad.item()
             self.assertAlmostEqual(actual, desired)
 
+    def test_nonnegsqrt(self):
+        vals = (-1.0, 0.0, 1.0, 2.0)
+        desireds = (0.0, 0.0, 1.0, sqrt(2.0))
+
+        for val, desired in zip(vals, desireds):
+            x = torch.tensor(val, requires_grad=True)
+            y = pystiche.nonnegsqrt(x)
+
+            actual = y.item()
+            self.assertAlmostEqual(actual, desired)
+
+    def test_nonnegsqrt_grad(self):
+        vals = (-1.0, 0.0, 1.0, 2.0)
+        desireds = (0.0, 0.0, 1.0 / 2.0, 1.0 / (2.0 * sqrt(2.0)))
+
+        for val, desired in zip(vals, desireds):
+            x = torch.tensor(val, requires_grad=True)
+            y = pystiche.nonnegsqrt(x)
+            y.backward()
+
+            actual = x.grad.item()
+            self.assertAlmostEqual(actual, desired)
+
     def test_batch_gram_matrix(self):
         size = 100
 
