52c8d35e5d7032a2ea3cbc47694465b980f56a75
Philip Meier <github.pmeier@posteo.de>
2020-06-03 14:21:07 +0200

    initial test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -178,3 +178,9 @@ html_theme = "sphinx_rtd_theme"
 # to this directory. They are copied after the builtin static files, so a file named
 # "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
+
+
+# -- Options for Latex output ----------------------------------------------------------
+
+latex_additional_files = ["pystiche.sty"]
+latex_elements = {"preamble": r"\usepackage{pystiche}"}
