e19c6e63e341f522834a9d3a4c05a58078e2c188
Philip Meier <github.pmeier@posteo.de>
2020-06-05 14:32:35 +0200

    remove accidentally added file

--- dummy.py
+++
@@ -1,9 +0,0 @@
-from pystiche.optim import default_logger
-
-logger = default_logger("blub")
-
-logger.debug("debug")
-logger.info("info")
-logger.warning("warning")
-logger.error("error")
-logger.error("critical")
