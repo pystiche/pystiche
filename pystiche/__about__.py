290eb8153e43426275a93dbe52d222c8b866f4eb
Philip Meier <github.pmeier@posteo.de>
2020-06-05 14:45:32 +0200

    bump version from 0.4.0 to 0.5.0

--- pystiche/__about__.py
+++ pystiche/__about__.py
@@ -13,13 +13,13 @@ __all__ = [
 
 __name__ = "pystiche"
 __description__ = "pystiche is a framework for Neural Style Transfer (NST) algorithms built upon PyTorch"
-__base_version__ = __version__ = "0.4.0"
+__base_version__ = __version__ = "0.5.0"
 __url__ = "https://github.com/pmeier/pystiche"
 __license__ = "BSD 3-Clause"
 __author__ = "Philip Meier"
 __author_email__ = "github.pmeier@posteo.de"
 
-_IS_DEV_VERSION = False
+_IS_DEV_VERSION = True
 
 if _IS_DEV_VERSION:
     __version__ += "+dev"
