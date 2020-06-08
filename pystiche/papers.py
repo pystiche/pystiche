ad047f68ff1929630d2694ed658290a23a3ecf83
Philip Meier <github.pmeier@posteo.de>
2020-06-08 11:34:26 +0200

    remove outsourced code

---
+++ pystiche/papers.py
@@ -0,0 +1,6 @@
+msg = (
+    "The sub-package pystiche.papers was outsourced in pystiche==0.5.0 to a seperate "
+    "package. Please install pystiche_papers "
+    "(https://github.com/pmeier/pystiche_papers)."
+)
+raise RuntimeError(msg)
