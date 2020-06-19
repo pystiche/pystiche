466becb632894fd8fb4fc8c6400aa9c70942e067
Philip Meier <github.pmeier@posteo.de>
2020-06-19 12:31:15 +0200

    use pytorch_wheel_installer for CI

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-REQUIREMENTS_FILE=ci_torch_cpu_requirements.txt
-python3 gen_torch_cpu_requirements.py --file $REQUIREMENTS_FILE
-pip3 install -r $REQUIREMENTS_FILE
+pip3 install pytorch_wheel_installer
+pwi --pip-cmd "pip3 install"
 
 pip3 install -r requirements-test.txt
 
