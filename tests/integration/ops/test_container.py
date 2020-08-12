63890de8fd1c5bd06712a710b5e312b51c31ec2a
Philip Meier <github.pmeier@posteo.de>
2020-08-12 16:09:32 +0200

    refactor tests for pystiche.ops (#372)

---
+++ tests/integration/ops/test_container.py
@@ -0,0 +1,304 @@
+import pytest
+import pytorch_testing_utils as ptu
+
+import torch
+from torch import nn
+
+import pystiche
+from pystiche import enc, ops
+
+from tests.asserts import assert_named_modules_identical
+
+
+def test_OperatorContainer():
+    class TestOperator(ops.Operator):
+        def process_input_image(self, image):
+            pass
+
+    named_ops = [(str(idx), TestOperator()) for idx in range(3)]
+    op_container = ops.OperatorContainer(named_ops)
+
+    actuals = op_container.named_children()
+    desireds = named_ops
+    assert_named_modules_identical(actuals, desireds)
+
+
+def test_OperatorContainer_get_image_or_guide(subtests):
+    class RegularizationTestOperator(ops.PixelRegularizationOperator):
+        def input_image_to_repr(self, image):
+            pass
+
+        def calculate_score(self, input_repr):
+            pass
+
+    class ComparisonTestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            return image, None
+
+        def input_image_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    def get_container():
+        return ops.OperatorContainer(
+            (
+                ("regularization", RegularizationTestOperator()),
+                ("comparison", ComparisonTestOperator()),
+            )
+        )
+
+    torch.manual_seed(0)
+    image_or_guide = torch.rand(1, 3, 128, 128)
+
+    with subtests.test("no image or guide"):
+        container = get_container()
+
+        with pytest.raises(RuntimeError):
+            container.get_input_guide()
+
+    with subtests.test("mismatching images or guides"):
+        container = get_container()
+        container.regularization.set_input_guide(image_or_guide)
+        container.comparison.set_input_guide(-image_or_guide)
+
+        with pytest.raises(RuntimeError):
+            container.get_input_guide()
+
+    with subtests.test("get_target_guide"):
+        container = get_container()
+        container.comparison.set_target_guide(image_or_guide)
+
+        actual = container.get_target_guide()
+        desired = image_or_guide
+        ptu.assert_allclose(actual, desired)
+
+    with subtests.test("get_target_image"):
+        container = get_container()
+        container.comparison.set_target_image(image_or_guide)
+
+        actual = container.get_target_image()
+        desired = image_or_guide
+        ptu.assert_allclose(actual, desired)
+
+    with subtests.test("get_input_guide"):
+        container = get_container()
+        container.regularization.set_input_guide(image_or_guide)
+        container.comparison.set_input_guide(image_or_guide)
+
+        actual = container.get_input_guide()
+        desired = image_or_guide
+        ptu.assert_allclose(actual, desired)
+
+
+def test_OperatorContainer_set_image_or_guide(subtests):
+    class RegularizationTestOperator(ops.PixelRegularizationOperator):
+        def input_image_to_repr(self, image):
+            pass
+
+        def calculate_score(self, input_repr):
+            pass
+
+    class ComparisonTestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            return image, None
+
+        def input_image_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    def get_container():
+        return ops.OperatorContainer(
+            (
+                ("regularization", RegularizationTestOperator()),
+                ("comparison", ComparisonTestOperator()),
+            )
+        )
+
+    torch.manual_seed(0)
+    image_or_guide = torch.rand(1, 3, 128, 128)
+
+    with subtests.test("set_target_guide"):
+        container = get_container()
+        container.set_target_guide(image_or_guide, recalc_repr=False)
+
+        actual = container.comparison.target_guide
+        desired = image_or_guide
+        ptu.assert_allclose(actual, desired)
+
+    with subtests.test("set_target_guide"):
+        container = get_container()
+        container.set_target_image(image_or_guide)
+
+        actual = container.comparison.target_image
+        desired = image_or_guide
+        ptu.assert_allclose(actual, desired)
+
+    with subtests.test("set_input_guide"):
+        container = get_container()
+        container.set_input_guide(image_or_guide)
+
+        actual = container.regularization.input_guide
+        desired = image_or_guide
+        ptu.assert_allclose(actual, desired)
+
+        actual = container.comparison.input_guide
+        desired = image_or_guide
+        ptu.assert_allclose(actual, desired)
+
+
+def test_OperatorContainer_call():
+    class TestOperator(ops.Operator):
+        def __init__(self, bias):
+            super().__init__()
+            self.bias = bias
+
+        def process_input_image(self, image):
+            return image + self.bias
+
+    input = torch.tensor(0.0)
+
+    named_ops = [(str(idx), TestOperator(idx + 1.0)) for idx in range(3)]
+    op_container = ops.OperatorContainer(named_ops)
+
+    actual = op_container(input)
+    desired = pystiche.LossDict([(name, input + op.bias) for name, op in named_ops])
+    ptu.assert_allclose(actual, desired)
+
+
+def test_SameOperatorContainer():
+    class TestOperator(ops.Operator):
+        def process_input_image(self, image):
+            pass
+
+    def get_op(name, score_weight):
+        return TestOperator()
+
+    names = [str(idx) for idx in range(3)]
+    same_operator_container = ops.SameOperatorContainer(names, get_op)
+
+    for name in names:
+        op = getattr(same_operator_container, name)
+        assert isinstance(op, TestOperator)
+
+
+def test_SameOperatorContainer_op_weights_str():
+    class TestOperator(ops.Operator):
+        def process_input_image(self, image):
+            pass
+
+    def get_op(name, score_weight):
+        return TestOperator(score_weight=score_weight)
+
+    names = [str(idx) for idx in range(3)]
+    op_weights_config = ("sum", "mean")
+
+    desireds = (float(len(names)), 1.0)
+    for op_weights, desired in zip(op_weights_config, desireds):
+        same_operator_container = ops.SameOperatorContainer(
+            names, get_op, op_weights=op_weights
+        )
+        actual = sum(
+            [getattr(same_operator_container, name).score_weight for name in names]
+        )
+        assert actual == ptu.approx(desired)
+
+    with pytest.raises(ValueError):
+        ops.SameOperatorContainer(
+            names, get_op, op_weights="invalid",
+        )
+
+
+def test_SameOperatorContainer_op_weights_seq():
+    class TestOperator(ops.Operator):
+        def process_input_image(self, image):
+            pass
+
+    def get_op(name, score_weight):
+        return TestOperator(score_weight=score_weight)
+
+    names, op_weights = zip(*[(str(idx), float(idx) + 1.0) for idx in range(3)])
+
+    same_operator_container = ops.SameOperatorContainer(
+        names, get_op, op_weights=op_weights
+    )
+
+    for name, score_weight in zip(names, op_weights):
+        actual = getattr(same_operator_container, name).score_weight
+        desired = score_weight
+        assert actual == ptu.approx(desired)
+
+
+def test_MultiLayerEncodingOperator():
+    class TestOperator(ops.EncodingRegularizationOperator):
+        def input_enc_to_repr(self, image):
+            pass
+
+        def calculate_score(self, input_repr):
+            pass
+
+    def get_encoding_op(encoder, score_weight):
+        return TestOperator(encoder, score_weight=score_weight)
+
+    layers = [str(index) for index in range(3)]
+    modules = [(layer, nn.Module()) for layer in layers]
+    multi_layer_encoder = enc.MultiLayerEncoder(modules)
+
+    multi_layer_enc_op = ops.MultiLayerEncodingOperator(
+        multi_layer_encoder, layers, get_encoding_op
+    )
+
+    for layer in layers:
+        op = getattr(multi_layer_enc_op, layer)
+        assert isinstance(op.encoder, enc.SingleLayerEncoder)
+        assert op.encoder.layer == layer
+        assert op.encoder.multi_layer_encoder is multi_layer_encoder
+
+
+def test_MultiRegionOperator_set_regional_image_or_guide(subtests):
+    class TestOperator(ops.PixelComparisonOperator):
+        def __init__(self, bias, score_weight=1e0):
+            super().__init__(score_weight=score_weight)
+            self.bias = bias
+
+        def target_image_to_repr(self, image):
+            return image + self.bias, None
+
+        def input_image_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    def get_op(name, score_weight):
+        return TestOperator(float(name), score_weight=score_weight)
+
+    regions = [str(idx) for idx in range(3)]
+    torch.manual_seed(0)
+    regional_images_or_guides = [
+        (region, torch.rand(1, 3, 128, 128)) for region in regions
+    ]
+
+    def get_multi_region_operator():
+        return ops.MultiRegionOperator(regions, get_op)
+
+    methods_and_desired_attrs = (
+        ("set_regional_target_guide", "target_guide"),
+        ("set_regional_target_image", "target_image"),
+        ("set_regional_input_guide", "input_guide"),
+    )
+
+    for method, desired_attr in methods_and_desired_attrs:
+        with subtests.test(method):
+            multi_region_operator = get_multi_region_operator()
+
+            for region, image_or_guide in regional_images_or_guides:
+                getattr(multi_region_operator, method)(region, image_or_guide)
+
+            for region, image_or_guide in regional_images_or_guides:
+                actual = getattr(getattr(multi_region_operator, region), desired_attr)
+                desired = image_or_guide
+                ptu.assert_allclose(actual, desired)
---
+++ tests/integration/ops/test_op.py
@@ -0,0 +1,485 @@
+import pytest
+import pytorch_testing_utils as ptu
+
+import torch
+from torch import nn
+
+from pystiche import enc, ops
+
+
+def test_Operator_set_input_guide():
+    class TestOperator(ops.Operator):
+        def process_input_image(self, image):
+            pass
+
+    torch.manual_seed(0)
+    guide = torch.rand(1, 1, 32, 32)
+
+    test_op = TestOperator()
+    assert not test_op.has_input_guide
+
+    test_op.set_input_guide(guide)
+    assert test_op.has_input_guide
+
+    actual = test_op.input_guide
+    desired = guide
+    ptu.assert_allclose(actual, desired)
+
+
+def test_Operator_apply_guide():
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 32, 32)
+    guide = torch.rand(1, 1, 32, 32)
+
+    actual = ops.Operator.apply_guide(image, guide)
+    desired = image * guide
+    ptu.assert_allclose(actual, desired)
+
+
+def test_Operator_named_operators():
+    # TODO: add
+    pass
+
+
+def test_Operator_call():
+    class TestOperator(ops.Operator):
+        def process_input_image(self, image):
+            return image + 1.0
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 128, 128)
+
+    test_op = TestOperator(score_weight=2.0)
+
+    actual = test_op(image)
+    desired = (image + 1.0) * 2.0
+    ptu.assert_allclose(actual, desired)
+
+
+def test_PixelRegularizationOperator_call():
+    class TestOperator(ops.PixelRegularizationOperator):
+        def input_image_to_repr(self, image):
+            return image * 2.0
+
+        def calculate_score(self, input_repr):
+            return input_repr + 1.0
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 128, 128)
+
+    test_op = TestOperator()
+
+    actual = test_op(image)
+    desired = image * 2.0 + 1.0
+    ptu.assert_allclose(actual, desired)
+
+
+def test_PixelRegularizationOperator_call_guided():
+    class TestOperator(ops.PixelRegularizationOperator):
+        def input_image_to_repr(self, image):
+            return image * 2.0
+
+        def calculate_score(self, input_repr):
+            return input_repr + 1.0
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 32, 32)
+    guide = torch.rand(1, 1, 32, 32)
+
+    test_op = TestOperator()
+    test_op.set_input_guide(guide)
+
+    actual = test_op(image)
+    desired = TestOperator.apply_guide(image, guide) * 2.0 + 1.0
+    ptu.assert_allclose(actual, desired)
+
+
+def test_EncodingRegularizationOperator_call():
+    class TestOperator(ops.EncodingRegularizationOperator):
+        def input_enc_to_repr(self, image):
+            return image * 2.0
+
+        def calculate_score(self, input_repr):
+            return input_repr + 1.0
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 128, 128)
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+
+    test_op = TestOperator(encoder)
+
+    actual = test_op(image)
+    desired = encoder(image) * 2.0 + 1.0
+    ptu.assert_allclose(actual, desired)
+
+
+def test_EncodingRegularizationOperator_call_guided():
+    class TestOperator(ops.EncodingRegularizationOperator):
+        def input_enc_to_repr(self, image):
+            return image * 2.0
+
+        def calculate_score(self, input_repr):
+            return input_repr + 1.0
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 32, 32)
+    guide = torch.rand(1, 3, 32, 32)
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+    enc_guide = encoder.propagate_guide(guide)
+
+    test_op = TestOperator(encoder)
+    test_op.set_input_guide(guide)
+
+    actual = test_op(image)
+    desired = TestOperator.apply_guide(encoder(image), enc_guide) * 2.0 + 1.0
+    ptu.assert_allclose(actual, desired)
+
+
+def test_PixelComparisonOperator_set_target_guide():
+    class TestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            repr = image * 2.0
+            ctx = torch.norm(image)
+            return repr, ctx
+
+        def input_image_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 32, 32)
+    guide = torch.rand(1, 1, 32, 32)
+
+    test_op = TestOperator()
+    test_op.set_target_image(image)
+    assert not test_op.has_target_guide
+
+    test_op.set_target_guide(guide)
+    assert test_op.has_target_guide
+
+    actual = test_op.target_guide
+    desired = guide
+    ptu.assert_allclose(actual, desired)
+
+    actual = test_op.target_image
+    desired = image
+    ptu.assert_allclose(actual, desired)
+
+
+def test_PixelComparisonOperator_set_target_guide_without_recalc():
+    class TestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            repr = image * 2.0
+            ctx = torch.norm(image)
+            return repr, ctx
+
+        def input_image_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    repr = torch.rand(1, 3, 32, 32)
+    guide = torch.rand(1, 1, 32, 32)
+
+    test_op = TestOperator()
+    test_op.register_buffer("target_repr", repr)
+    test_op.set_target_guide(guide, recalc_repr=False)
+
+    actual = test_op.target_repr
+    desired = repr
+    ptu.assert_allclose(actual, desired)
+
+
+def test_PixelComparisonOperator_set_target_image():
+    class TestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            repr = image * 2.0
+            ctx = torch.norm(image)
+            return repr, ctx
+
+        def input_image_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 128, 128)
+
+    test_op = TestOperator()
+    assert not test_op.has_target_image
+
+    test_op.set_target_image(image)
+    assert test_op.has_target_image
+
+    actual = test_op.target_image
+    desired = image
+    ptu.assert_allclose(actual, desired)
+
+    actual = test_op.target_repr
+    desired = image * 2.0
+    ptu.assert_allclose(actual, desired)
+
+    actual = test_op.ctx
+    desired = torch.norm(image)
+    ptu.assert_allclose(actual, desired)
+
+
+def test_PixelComparisonOperator_call():
+    class TestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            repr = image + 1.0
+            return repr, None
+
+        def input_image_to_repr(self, image, ctx):
+            return image + 2.0
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            return input_repr * target_repr
+
+    torch.manual_seed(0)
+    target_image = torch.rand(1, 3, 128, 128)
+    input_image = torch.rand(1, 3, 128, 128)
+
+    test_op = TestOperator()
+    test_op.set_target_image(target_image)
+
+    actual = test_op(input_image)
+    desired = (target_image + 1.0) * (input_image + 2.0)
+    ptu.assert_allclose(actual, desired)
+
+
+def test_PixelComparisonOperator_call_no_target():
+    class TestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            pass
+
+        def input_image_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    input_image = torch.rand(1, 3, 128, 128)
+
+    test_op = TestOperator()
+
+    with pytest.raises(RuntimeError):
+        test_op(input_image)
+
+
+def test_PixelComparisonOperator_call_guided():
+    class TestOperator(ops.PixelComparisonOperator):
+        def target_image_to_repr(self, image):
+            repr = image + 1.0
+            return repr, None
+
+        def input_image_to_repr(self, image, ctx):
+            return image + 2.0
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            return input_repr * target_repr
+
+    torch.manual_seed(0)
+    target_image = torch.rand(1, 3, 32, 32)
+    target_guide = torch.rand(1, 1, 32, 32)
+    input_image = torch.rand(1, 3, 32, 32)
+    input_guide = torch.rand(1, 1, 32, 32)
+
+    test_op = TestOperator()
+    test_op.set_target_guide(target_guide)
+    test_op.set_target_image(target_image)
+    test_op.set_input_guide(input_guide)
+
+    actual = test_op(input_image)
+    desired = (TestOperator.apply_guide(target_image, target_guide) + 1.0) * (
+        TestOperator.apply_guide(input_image, input_guide) + 2.0
+    )
+    ptu.assert_allclose(actual, desired)
+
+
+def test_EncodingComparisonOperator_set_target_guide():
+    class TestOperator(ops.EncodingComparisonOperator):
+        def target_enc_to_repr(self, image):
+            repr = image * 2.0
+            ctx = torch.norm(image)
+            return repr, ctx
+
+        def input_enc_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 32, 32)
+    guide = torch.rand(1, 1, 32, 32)
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+    enc_guide = encoder.propagate_guide(guide)
+
+    test_op = TestOperator(encoder)
+    test_op.set_target_image(image)
+    assert not test_op.has_target_guide
+
+    test_op.set_target_guide(guide)
+    assert test_op.has_target_guide
+
+    actual = test_op.target_guide
+    desired = guide
+    ptu.assert_allclose(actual, desired)
+
+    actual = test_op.target_enc_guide
+    desired = enc_guide
+    ptu.assert_allclose(actual, desired)
+
+    actual = test_op.target_image
+    desired = image
+    ptu.assert_allclose(actual, desired)
+
+
+def test_EncodingComparisonOperator_set_target_guide_without_recalc():
+    class TestOperator(ops.EncodingComparisonOperator):
+        def target_enc_to_repr(self, image):
+            repr = image * 2.0
+            ctx = torch.norm(image)
+            return repr, ctx
+
+        def input_enc_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    repr = torch.rand(1, 3, 32, 32)
+    guide = torch.rand(1, 1, 32, 32)
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+
+    test_op = TestOperator(encoder)
+    test_op.register_buffer("target_repr", repr)
+    test_op.set_target_guide(guide, recalc_repr=False)
+
+    actual = test_op.target_repr
+    desired = repr
+    ptu.assert_allclose(actual, desired)
+
+
+def test_EncodingComparisonOperator_set_target_image():
+    class TestOperator(ops.EncodingComparisonOperator):
+        def target_enc_to_repr(self, image):
+            repr = image * 2.0
+            ctx = torch.norm(image)
+            return repr, ctx
+
+        def input_enc_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    image = torch.rand(1, 3, 128, 128)
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+
+    test_op = TestOperator(encoder)
+    assert not test_op.has_target_image
+
+    test_op.set_target_image(image)
+    assert test_op.has_target_image
+
+    actual = test_op.target_image
+    desired = image
+    ptu.assert_allclose(actual, desired)
+
+    actual = test_op.target_repr
+    desired = encoder(image) * 2.0
+    ptu.assert_allclose(actual, desired)
+
+    actual = test_op.ctx
+    desired = torch.norm(encoder(image))
+    ptu.assert_allclose(actual, desired)
+
+
+def test_EncodingComparisonOperator_call():
+    class TestOperator(ops.EncodingComparisonOperator):
+        def target_enc_to_repr(self, image):
+            repr = image + 1.0
+            return repr, None
+
+        def input_enc_to_repr(self, image, ctx):
+            return image + 2.0
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            return input_repr * target_repr
+
+    torch.manual_seed(0)
+    target_image = torch.rand(1, 3, 128, 128)
+    input_image = torch.rand(1, 3, 128, 128)
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+
+    test_op = TestOperator(encoder)
+    test_op.set_target_image(target_image)
+
+    actual = test_op(input_image)
+    desired = (encoder(target_image) + 1.0) * (encoder(input_image) + 2.0)
+    ptu.assert_allclose(actual, desired)
+
+
+def test_EncodingComparisonOperator_call_no_target():
+    class TestOperator(ops.EncodingComparisonOperator):
+        def target_enc_to_repr(self, image):
+            pass
+
+        def input_enc_to_repr(self, image, ctx):
+            pass
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            pass
+
+    torch.manual_seed(0)
+    input_image = torch.rand(1, 3, 128, 128)
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+
+    test_op = TestOperator(encoder)
+
+    with pytest.raises(RuntimeError):
+        test_op(input_image)
+
+
+def test_EncodingComparisonOperator_call_guided():
+    class TestOperator(ops.EncodingComparisonOperator):
+        def target_enc_to_repr(self, image):
+            repr = image + 1.0
+            return repr, None
+
+        def input_enc_to_repr(self, image, ctx):
+            return image + 2.0
+
+        def calculate_score(self, input_repr, target_repr, ctx):
+            return input_repr * target_repr
+
+    torch.manual_seed(0)
+    target_image = torch.rand(1, 3, 32, 32)
+    input_image = torch.rand(1, 3, 32, 32)
+    target_guide = torch.rand(1, 1, 32, 32)
+    input_guide = torch.rand(1, 1, 32, 32)
+
+    encoder = enc.SequentialEncoder((nn.Conv2d(3, 3, 1),))
+    target_enc_guide = encoder.propagate_guide(target_guide)
+    input_enc_guide = encoder.propagate_guide(input_guide)
+
+    test_op = TestOperator(encoder)
+    test_op.set_target_guide(target_guide)
+    test_op.set_target_image(target_image)
+    test_op.set_input_guide(input_guide)
+
+    actual = test_op(input_image)
+    desired = (
+        TestOperator.apply_guide(encoder(target_image), target_enc_guide) + 1.0
+    ) * (TestOperator.apply_guide(encoder(input_image), input_enc_guide) + 2.0)
+    ptu.assert_allclose(actual, desired)
