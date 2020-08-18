66084ed513bba69412f605c0cde2cd568d3515c4
Philip Meier <github.pmeier@posteo.de>
2020-08-18 15:36:26 +0200

    make package importable without installation (#388)

--- pystiche/__init__.py
+++ pystiche/__init__.py
@@ -1,4 +1,7 @@
-from ._version import version as __version__  # type: ignore[import]
+try:
+    from ._version import version as __version__  # type: ignore[import]
+except ImportError:
+    __version__ = "UNKNOWN"
 
 from .core import *
 
