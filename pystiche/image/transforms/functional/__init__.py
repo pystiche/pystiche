c5983ec552603b40129cb1b5abba6e685e03b731
Philip Meier <github.pmeier@posteo.de>
2020-10-29 09:13:03 +0100

    deprecate transforms and transforms.functional

--- pystiche/image/transforms/functional/__init__.py
+++ pystiche/image/transforms/functional/__init__.py
@@ -1,5 +1,16 @@
+import warnings
+
+from pystiche.misc import build_deprecation_message
+
 from ._color import *
 from ._crop import *
 from ._misc import *
 from ._motif import *
 from ._resize import *
+
+msg = build_deprecation_message(
+    "Using functionality from pystiche.image.transforms.functional",
+    "0.7.0",
+    info="See https://github.com/pmeier/pystiche/issues/382 for details.",
+)
+warnings.warn(msg, UserWarning)
