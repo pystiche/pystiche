1eb6ca994da3647fccea03dc1a2c73be01824d5d
Philip Meier <github.pmeier@posteo.de>
2020-11-04 11:29:49 +0100

    remove tests for logging

--- tests/integration/test_optim.py
+++ tests/integration/test_optim.py
@@ -11,8 +11,6 @@ from torch.utils import data
 
 from pystiche import loss, ops, optim
 
-from tests import asserts
-
 skip_if_py38 = pytest.mark.skipif(
     sys.version_info >= (3, 8),
     reason=(
@@ -79,22 +77,6 @@ def test_default_image_optim_loop_processing(optim_asset_loader):
 
 
 @skip_if_py38
-def test_default_image_optim_loop_logging_smoke(caplog, optim_asset_loader):
-    asset = optim_asset_loader("default_image_optim_loop")
-
-    num_steps = 1
-    optim_logger = optim.OptimLogger()
-    log_fn = optim.default_image_optim_log_fn(optim_logger, log_freq=1)
-    with asserts.assert_logs(caplog, logger=optim_logger):
-        optim.default_image_optim_loop(
-            asset.input.image,
-            asset.input.criterion,
-            num_steps=num_steps,
-            log_fn=log_fn,
-        )
-
-
-@skip_if_py38
 def test_default_image_pyramid_optim_loop(optim_asset_loader):
     asset = optim_asset_loader("default_image_pyramid_optim_loop")
 
@@ -124,24 +106,6 @@ def test_default_image_pyramid_optim_loop_processing(optim_asset_loader):
     )
     desired = asset.output.image
     ptu.assert_allclose(actual, desired, rtol=1e-4)
-
-
-@skip_if_py38
-def test_default_image_pyramid_optim_loop_logging_smoke(caplog, optim_asset_loader):
-    asset = optim_asset_loader("default_image_pyramid_optim_loop")
-
-    optim_logger = optim.OptimLogger()
-    log_freq = max(level.num_steps for level in asset.input.pyramid._levels) + 1
-    log_fn = optim.default_image_optim_log_fn(optim_logger, log_freq=log_freq)
-
-    with asserts.assert_logs(caplog, logger=optim_logger):
-        optim.default_image_pyramid_optim_loop(
-            asset.input.image,
-            asset.input.criterion,
-            asset.input.pyramid,
-            logger=optim_logger,
-            log_fn=log_fn,
-        )
 
 
 def test_default_transformer_optimizer():
@@ -190,30 +154,6 @@ def test_default_transformer_optim_loop(optim_asset_loader):
 
 
 @skip_if_py38
-def test_default_transformer_optim_loop_logging_smoke(caplog, optim_asset_loader):
-    asset = optim_asset_loader("default_transformer_optim_loop")
-
-    image_loader = asset.input.image_loader
-    criterion = asset.input.criterion
-    make_torch_ge_1_6_compatible(image_loader, criterion)
-
-    optim_logger = optim.OptimLogger()
-    log_fn = optim.default_transformer_optim_log_fn(
-        optim_logger, len(image_loader), log_freq=1
-    )
-
-    with asserts.assert_logs(caplog, logger=optim_logger):
-        optim.default_transformer_optim_loop(
-            image_loader,
-            asset.input.transformer,
-            criterion,
-            asset.input.criterion_update_fn,
-            logger=optim_logger,
-            log_fn=log_fn,
-        )
-
-
-@skip_if_py38
 def test_default_transformer_epoch_optim_loop(optim_asset_loader):
     asset = optim_asset_loader("default_transformer_epoch_optim_loop")
 
@@ -238,32 +178,6 @@ def test_default_transformer_epoch_optim_loop(optim_asset_loader):
     actual = tuple(transformer.parameters())
     desired = tuple(asset.output.transformer.parameters())
     ptu.assert_allclose(actual, desired, rtol=1e-4)
-
-
-@skip_if_py38
-def test_default_transformer_epoch_optim_loop_logging_smoke(caplog, optim_asset_loader):
-    asset = optim_asset_loader("default_transformer_epoch_optim_loop")
-
-    image_loader = asset.input.image_loader
-    criterion = asset.input.criterion
-    make_torch_ge_1_6_compatible(image_loader, criterion)
-
-    log_freq = len(image_loader) + 1
-    optim_logger = optim.OptimLogger()
-    log_fn = optim.default_transformer_optim_log_fn(
-        optim_logger, len(image_loader), log_freq=log_freq
-    )
-
-    with asserts.assert_logs(caplog, logger=optim_logger):
-        optim.default_transformer_epoch_optim_loop(
-            image_loader,
-            asset.input.transformer,
-            criterion,
-            asset.input.criterion_update_fn,
-            asset.input.epochs,
-            logger=optim_logger,
-            log_fn=log_fn,
-        )
 
 
 class Dataset(data.Dataset):
