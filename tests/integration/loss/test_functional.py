a22dbb7cff301ee8e210f6d9cd37b37c4acaff4b
sakshi kumari <56168294+sakshi-06@users.noreply.github.com>
2021-08-11 18:01:52 +0530

    Use TestFunctionalLoss for all the test methods in test_functional.py

--- tests/integration/loss/test_functional.py
+++ tests/integration/loss/test_functional.py
@@ -5,8 +5,8 @@ from torch.nn.functional import mse_loss
 
 import pystiche.ops.functional as F
 
-
-def test_mrf_loss():
+class TestFunctionalLoss:
+    def test_mrf_loss(self):
     torch.manual_seed(0)
     zero_patch = torch.zeros(4, 5, 6)
     one_patch = torch.ones(4, 5, 6)
@@ -20,7 +20,7 @@ def test_mrf_loss():
     ptu.assert_allclose(actual, desired)
 
 
-def test_mrf_loss_non_batched():
+def test_mrf_loss_non_batched(self):
     torch.manual_seed(0)
     zero_patch = torch.zeros(4, 5, 6)
     one_patch = torch.ones(4, 5, 6)
@@ -34,7 +34,7 @@ def test_mrf_loss_non_batched():
     ptu.assert_allclose(actual, desired)
 
 
-def test_value_range_loss_zero():
+def test_value_range_loss_zero(self):
     torch.manual_seed(0)
     input = torch.rand(1, 3, 128, 128)
 
@@ -43,7 +43,7 @@ def test_value_range_loss_zero():
     assert desired == ptu.approx(actual)
 
 
-def test_total_variation_loss():
+def test_total_variation_loss(self):
     def get_checkerboard(size):
         return (
             (
