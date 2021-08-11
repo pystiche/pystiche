34ab186dc7d3217486c826968a6217fc88ff06c2
sakshi kumari <56168294+sakshi-06@users.noreply.github.com>
2021-08-11 18:05:57 +0530

    Update test_functional.py

--- tests/integration/loss/test_functional.py
+++ tests/integration/loss/test_functional.py
@@ -7,56 +7,56 @@ import pystiche.ops.functional as F
 
 class TestFunctionalLoss:
     def test_mrf_loss(self):
-    torch.manual_seed(0)
-    zero_patch = torch.zeros(4, 5, 6)
-    one_patch = torch.ones(4, 5, 6)
-    rand_patch = torch.randn(4, 5, 6)
+        torch.manual_seed(0)
+        zero_patch = torch.zeros(4, 5, 6)
+        one_patch = torch.ones(4, 5, 6)
+        rand_patch = torch.randn(4, 5, 6)
 
-    input = torch.stack((rand_patch + 0.1, rand_patch * 0.9)).unsqueeze(0)
-    target = torch.stack((zero_patch, one_patch, rand_patch)).unsqueeze(0)
+        input = torch.stack((rand_patch + 0.1, rand_patch * 0.9)).unsqueeze(0)
+        target = torch.stack((zero_patch, one_patch, rand_patch)).unsqueeze(0)
 
-    actual = F.mrf_loss(input, target)
-    desired = mse_loss(input, torch.stack((rand_patch, rand_patch)).unsqueeze(0))
-    ptu.assert_allclose(actual, desired)
+        actual = F.mrf_loss(input, target)
+        desired = mse_loss(input, torch.stack((rand_patch, rand_patch)).unsqueeze(0))
+        ptu.assert_allclose(actual, desired)
+    
 
+    def test_mrf_loss_non_batched(self):
+        torch.manual_seed(0)
+        zero_patch = torch.zeros(4, 5, 6)
+        one_patch = torch.ones(4, 5, 6)
+        rand_patch = torch.randn(4, 5, 6)
 
-def test_mrf_loss_non_batched(self):
-    torch.manual_seed(0)
-    zero_patch = torch.zeros(4, 5, 6)
-    one_patch = torch.ones(4, 5, 6)
-    rand_patch = torch.randn(4, 5, 6)
+        input = torch.stack((rand_patch + 0.1, rand_patch * 0.9))
+        target = torch.stack((zero_patch, one_patch, rand_patch))
 
-    input = torch.stack((rand_patch + 0.1, rand_patch * 0.9))
-    target = torch.stack((zero_patch, one_patch, rand_patch))
-
-    actual = F.mrf_loss(input, target, batched_input=False)
-    desired = mse_loss(input, torch.stack((rand_patch, rand_patch)))
-    ptu.assert_allclose(actual, desired)
-
-
-def test_value_range_loss_zero(self):
-    torch.manual_seed(0)
-    input = torch.rand(1, 3, 128, 128)
-
-    actual = F.value_range_loss(input)
-    desired = 0.0
-    assert desired == ptu.approx(actual)
+        actual = F.mrf_loss(input, target, batched_input=False)
+        desired = mse_loss(input, torch.stack((rand_patch, rand_patch)))
+        ptu.assert_allclose(actual, desired)
 
 
-def test_total_variation_loss(self):
-    def get_checkerboard(size):
-        return (
-            (
-                torch.arange(size ** 2).view(size, size)
-                + torch.arange(size).view(size, 1)
-            )
-            % 2
-        ).bool()
+    def test_value_range_loss_zero(self):
+        torch.manual_seed(0)
+        input = torch.rand(1, 3, 128, 128)
 
-    size = 128
-    checkerboard = get_checkerboard(size)
-    input = checkerboard.float().view(1, 1, size, size).repeat(1, 3, 1, 1)
+        actual = F.value_range_loss(input)
+        desired = 0.0
+        assert desired == ptu.approx(actual)
 
-    actual = F.total_variation_loss(input)
-    desired = 2.0
-    assert desired == ptu.approx(actual)
+
+    def test_total_variation_loss(self):
+        def get_checkerboard(size):
+            return (
+                (
+                    torch.arange(size ** 2).view(size, size)
+                    + torch.arange(size).view(size, 1)
+                )
+                % 2
+            ).bool()
+
+        size = 128
+        checkerboard = get_checkerboard(size)
+        input = checkerboard.float().view(1, 1, size, size).repeat(1, 3, 1, 1)
+
+        actual = F.total_variation_loss(input)
+        desired = 2.0
+        assert desired == ptu.approx(actual)
