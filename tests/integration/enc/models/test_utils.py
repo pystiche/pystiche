74d7bc51b73e33c98135ee8bd721a34bc8c6137f
Pranjal Gulati <72960463+DevPranjal@users.noreply.github.com>
2021-08-20 18:19:24 +0530

    Cleaned up test suite for tests in demo and enc/models (#523)
    * Cleaned up test suite for tests in demo and enc/models
    
    * Applied suggestions and fix codecov

--- tests/integration/enc/models/test_utils.py
+++ tests/integration/enc/models/test_utils.py
@@ -14,35 +14,34 @@ from pystiche import enc
 from tests import asserts, mocks
 
 
-def test_select_url():
-    key = "available"
-    url = "https://pystiche.org"
+class TestSelectURL:
+    def test_main(self):
+        key = "available"
+        url = "https://pystiche.org"
 
-    assert enc.select_url({key: url}, key) == url
+        assert enc.select_url({key: url}, key) == url
 
+    def test_not_available(self):
+        key = "not_available"
+        urls = {}
 
-def test_select_url_not_available():
-    key = "not_available"
-    urls = {}
+        with pytest.raises(RuntimeError):
+            enc.select_url(urls, key)
 
-    with pytest.raises(RuntimeError):
-        enc.select_url(urls, key)
+    def test_custom_format(self):
+        key = "not_available"
+        urls = {}
 
+        def format(key):
+            return f"custom format for '{key}'"
 
-def test_select_url_custom_format():
-    key = "not_available"
-    urls = {}
+        expected = format(key)
 
-    def format(key):
-        return f"custom format for '{key}'"
+        with pytest.raises(RuntimeError) as exc_info:
+            enc.select_url(urls, key, format=format)
 
-    expected = format(key)
-
-    with pytest.raises(RuntimeError) as exc_info:
-        enc.select_url(urls, key, format=format)
-
-        actual = exc_info.value.args[0]
-        assert actual.endswith(expected)
+            actual = exc_info.value.args[0]
+            assert actual.endswith(expected)
 
 
 @pytest.fixture
@@ -113,109 +112,108 @@ def multi_layer_encoder_cls(
     return MockModelMultiLayerEncoder
 
 
-def test_ModelMultiLayerEncoder_internal_preprocessing(
-    subtests, frameworks, multi_layer_encoder_cls
-):
-    for framework in ("caffe",):
-        with subtests.test(framework=framework):
-            multi_layer_encoder = multi_layer_encoder_cls(
-                pretrained=False, framework=framework, internal_preprocessing=True
-            )
-            assert "preprocessing" in multi_layer_encoder
-            assert isinstance(
-                multi_layer_encoder.preprocessing, type(enc.get_preprocessor(framework))
-            )
-
-
-def test_ModelMultiLayerEncoder_pretrained(mocker, multi_layer_encoder_cls):
-    load_state_dict_from_url = mocker.patch(
-        mocks.make_mock_target(
-            "enc",
-            "models",
-            "utils",
-            "ModelMultiLayerEncoder",
-            "load_state_dict_from_url",
-        )
-    )
-
-    framework = "framework"
-    multi_layer_encoder_cls(
-        pretrained=True, framework=framework, internal_preprocessing=False
-    )
-
-    load_state_dict_from_url.assert_called_once_with(framework)
-
-
-def test_ModelMultiLayerEncoder_load_state_dict(
-    multi_layer_encoder_cls, other_multi_layer_encoder_state_dict,
-):
-    multi_layer_encoder = multi_layer_encoder_cls(pretrained=False)
-    multi_layer_encoder.load_state_dict(
-        other_multi_layer_encoder_state_dict, map_names=False
-    )
-
-    ptu.assert_allclose(
-        multi_layer_encoder.state_dict(), other_multi_layer_encoder_state_dict
-    )
-
-
-def test_ModelMultiLayerEncoder_load_state_dict_name_mapping(
-    multi_layer_encoder_cls,
-    multi_layer_encoder_state_dict,
-    other_multi_layer_encoder_state_dict,
-    multi_layer_encoder_state_dict_key_map,
-):
-    inv_key_map = {v: k for k, v in multi_layer_encoder_state_dict_key_map.items()}
-    inv_state_dict = OrderedDict(
-        (inv_key_map[name], tensor)
-        for name, tensor in other_multi_layer_encoder_state_dict.items()
-    )
-
-    multi_layer_encoder = multi_layer_encoder_cls(pretrained=False)
-    multi_layer_encoder.load_state_dict(inv_state_dict, map_names=True)
-
-    ptu.assert_allclose(
-        multi_layer_encoder.state_dict(), other_multi_layer_encoder_state_dict
-    )
-
-
-def test_ModelMultiLayerEncoder_repr(subtests, mocker, multi_layer_encoder_cls):
-    mocker.patch(
-        mocks.make_mock_target(
-            "enc",
-            "models",
-            "utils",
-            "ModelMultiLayerEncoder",
-            "load_state_dict_from_url",
-        )
-    )
-
-    framework = "framework"
-    internal_preprocessing = False
-    allow_inplace = True
-    cls = functools.partial(
-        multi_layer_encoder_cls,
-        framework=framework,
-        internal_preprocessing=internal_preprocessing,
-        allow_inplace=allow_inplace,
-    )
-
-    for pretrained in (True, False):
-        multi_layer_encoder = cls(pretrained=pretrained)
-        assert_property_in_repr_ = functools.partial(
-            asserts.assert_property_in_repr, repr(multi_layer_encoder)
-        )
-
-        with subtests.test(pretrained=pretrained):
-            if pretrained:
-                assert_property_in_repr_("framework", framework)
-            else:
-                assert_property_in_repr_("pretrained", False)
-
-            with subtests.test("internal_preprocessing"):
-                assert_property_in_repr_(
-                    "internal_preprocessing", internal_preprocessing
+class TestModelMultiLayerEncoder:
+    def test_internal_preprocessing(
+        self, subtests, frameworks, multi_layer_encoder_cls
+    ):
+        for framework in ("caffe",):
+            with subtests.test(framework=framework):
+                multi_layer_encoder = multi_layer_encoder_cls(
+                    pretrained=False, framework=framework, internal_preprocessing=True
+                )
+                assert "preprocessing" in multi_layer_encoder
+                assert isinstance(
+                    multi_layer_encoder.preprocessing,
+                    type(enc.get_preprocessor(framework)),
                 )
 
-            with subtests.test("allow_inplace"):
-                assert_property_in_repr_("allow_inplace", allow_inplace)
+    def test_pretrained(self, mocker, multi_layer_encoder_cls):
+        load_state_dict_from_url = mocker.patch(
+            mocks.make_mock_target(
+                "enc",
+                "models",
+                "utils",
+                "ModelMultiLayerEncoder",
+                "load_state_dict_from_url",
+            )
+        )
+
+        framework = "framework"
+        multi_layer_encoder_cls(
+            pretrained=True, framework=framework, internal_preprocessing=False
+        )
+
+        load_state_dict_from_url.assert_called_once_with(framework)
+
+    def test_load_state_dict(
+        self, multi_layer_encoder_cls, other_multi_layer_encoder_state_dict,
+    ):
+        multi_layer_encoder = multi_layer_encoder_cls(pretrained=False)
+        multi_layer_encoder.load_state_dict(
+            other_multi_layer_encoder_state_dict, map_names=False
+        )
+
+        ptu.assert_allclose(
+            multi_layer_encoder.state_dict(), other_multi_layer_encoder_state_dict
+        )
+
+    def test_load_state_dict_name_mapping(
+        self,
+        multi_layer_encoder_cls,
+        multi_layer_encoder_state_dict,
+        other_multi_layer_encoder_state_dict,
+        multi_layer_encoder_state_dict_key_map,
+    ):
+        inv_key_map = {v: k for k, v in multi_layer_encoder_state_dict_key_map.items()}
+        inv_state_dict = OrderedDict(
+            (inv_key_map[name], tensor)
+            for name, tensor in other_multi_layer_encoder_state_dict.items()
+        )
+
+        multi_layer_encoder = multi_layer_encoder_cls(pretrained=False)
+        multi_layer_encoder.load_state_dict(inv_state_dict, map_names=True)
+
+        ptu.assert_allclose(
+            multi_layer_encoder.state_dict(), other_multi_layer_encoder_state_dict
+        )
+
+    def test_repr(self, subtests, mocker, multi_layer_encoder_cls):
+        mocker.patch(
+            mocks.make_mock_target(
+                "enc",
+                "models",
+                "utils",
+                "ModelMultiLayerEncoder",
+                "load_state_dict_from_url",
+            )
+        )
+
+        framework = "framework"
+        internal_preprocessing = False
+        allow_inplace = True
+        cls = functools.partial(
+            multi_layer_encoder_cls,
+            framework=framework,
+            internal_preprocessing=internal_preprocessing,
+            allow_inplace=allow_inplace,
+        )
+
+        for pretrained in (True, False):
+            multi_layer_encoder = cls(pretrained=pretrained)
+            assert_property_in_repr_ = functools.partial(
+                asserts.assert_property_in_repr, repr(multi_layer_encoder)
+            )
+
+            with subtests.test(pretrained=pretrained):
+                if pretrained:
+                    assert_property_in_repr_("framework", framework)
+                else:
+                    assert_property_in_repr_("pretrained", False)
+
+                with subtests.test("internal_preprocessing"):
+                    assert_property_in_repr_(
+                        "internal_preprocessing", internal_preprocessing
+                    )
+
+                with subtests.test("allow_inplace"):
+                    assert_property_in_repr_("allow_inplace", allow_inplace)
--- tests/integration/enc/models/test_vgg.py
+++ tests/integration/enc/models/test_vgg.py
@@ -30,101 +30,104 @@ vgg_multi_layer_encoder = enc.vgg11_bn_multi_layer_encoder
 vgg = MODELS["vgg11_bn"]
 
 
-@pytest.mark.slow
-def test_vgg_pretrained(mocker):
-    state_dict = vgg(pretrained=False).state_dict()
-    mocker.patch(
-        mocks.make_mock_target(
-            "enc", "models", "vgg", "hub", "load_state_dict_from_url"
-        ),
-        return_value=state_dict,
-    )
+class TestVGG:
+    @pytest.mark.slow
+    def test_pretrained(self, mocker):
+        state_dict = vgg(pretrained=False).state_dict()
+        mocker.patch(
+            mocks.make_mock_target(
+                "enc", "models", "vgg", "hub", "load_state_dict_from_url"
+            ),
+            return_value=state_dict,
+        )
 
-    model = vgg(pretrained=True)
-    ptu.assert_allclose(model.state_dict(), state_dict)
+        model = vgg(pretrained=True)
+        ptu.assert_allclose(model.state_dict(), state_dict)
 
-
-def test_vgg_pretrained_num_classes_mismatch():
-    with pytest.raises(RuntimeError):
-        vgg(pretrained=True, num_classes=1001)
-
-
-@pytest.mark.slow
-def test_VGGMultiLayerEncoder_unknown_arch():
-    arch = "unknown"
-    with pytest.raises(ValueError):
-        enc.VGGMultiLayerEncoder(arch, pretrained=False, internal_preprocessing=False)
+    def test_pretrained_num_classes_mismatch(self):
+        with pytest.raises(RuntimeError):
+            vgg(pretrained=True, num_classes=1001)
 
 
-@pytest.mark.slow
-def test_vgg_multi_layer_encoder_smoke(
-    subtests, vgg_archs, vgg_multi_layer_encoder_loaders,
-):
-    for arch, loader in zip(vgg_archs, vgg_multi_layer_encoder_loaders):
-        with subtests.test(arch=arch):
-            multi_layer_encoder = loader(pretrained=False)
-            assert isinstance(multi_layer_encoder, enc.vgg.VGGMultiLayerEncoder)
-
-            with subtests.test("repr"):
-                asserts.assert_property_in_repr(repr(multi_layer_encoder), "arch", arch)
-
-
-@pytest.mark.large_download
-@pytest.mark.slow
-@pytest.mark.flaky
-def test_vgg_multi_layer_encoder(
-    subtests, vgg_archs, vgg_multi_layer_encoder_loaders, enc_asset_loader
-):
-    for arch, loader in zip(vgg_archs, vgg_multi_layer_encoder_loaders):
-        with subtests.test(arch=arch):
-            asset = enc_asset_loader(arch)
-
-            multi_layer_encoder = loader(
-                pretrained=True,
-                weights="torch",
-                preprocessing=False,
-                allow_inplace=False,
+class TestVGGMultiLayerEncoder:
+    @pytest.mark.slow
+    def test_unknown_arch(self):
+        arch = "unknown"
+        with pytest.raises(ValueError):
+            enc.VGGMultiLayerEncoder(
+                arch, pretrained=False, internal_preprocessing=False
             )
-            layers = tuple(multi_layer_encoder.children_names())
-            with torch.no_grad():
-                encs = multi_layer_encoder(asset.input.image, layers)
 
-            actual = dict(
-                zip(
-                    layers,
-                    [
-                        pystiche.TensorKey(x, precision=asset.params.precision)
-                        for x in encs
-                    ],
+    @pytest.mark.slow
+    def test_smoke(
+        self, subtests, vgg_archs, vgg_multi_layer_encoder_loaders,
+    ):
+        for arch, loader in zip(vgg_archs, vgg_multi_layer_encoder_loaders):
+            with subtests.test(arch=arch):
+                multi_layer_encoder = loader(pretrained=False)
+                assert isinstance(multi_layer_encoder, enc.vgg.VGGMultiLayerEncoder)
+
+                with subtests.test("repr"):
+                    asserts.assert_property_in_repr(
+                        repr(multi_layer_encoder), "arch", arch
+                    )
+
+    @pytest.mark.large_download
+    @pytest.mark.slow
+    @pytest.mark.flaky
+    def test_main(
+        self, subtests, vgg_archs, vgg_multi_layer_encoder_loaders, enc_asset_loader
+    ):
+        for arch, loader in zip(vgg_archs, vgg_multi_layer_encoder_loaders):
+            with subtests.test(arch=arch):
+                asset = enc_asset_loader(arch)
+
+                multi_layer_encoder = loader(
+                    pretrained=True,
+                    weights="torch",
+                    preprocessing=False,
+                    allow_inplace=False,
                 )
-            )
-            desired = asset.output.enc_keys
-            assert actual == desired
+                layers = tuple(multi_layer_encoder.children_names())
+                with torch.no_grad():
+                    encs = multi_layer_encoder(asset.input.image, layers)
 
+                actual = dict(
+                    zip(
+                        layers,
+                        [
+                            pystiche.TensorKey(x, precision=asset.params.precision)
+                            for x in encs
+                        ],
+                    )
+                )
+                desired = asset.output.enc_keys
+                assert actual == desired
 
-@pytest.mark.slow
-def test_vgg_multi_layer_encoder_state_dict_url(subtests, vgg_archs, frameworks):
-    def should_be_available(arch, framework):
-        if framework == "caffe" and arch in ("vgg16", "vgg19"):
-            return True
+    @pytest.mark.slow
+    def test_state_dict_url(self, subtests, vgg_archs, frameworks):
+        def should_be_available(arch, framework):
+            if framework == "caffe" and arch in ("vgg16", "vgg19"):
+                return True
 
-        return framework == "torch"
+            return framework == "torch"
 
-    multi_layer_encoder = vgg_multi_layer_encoder(pretrained=False)
+        multi_layer_encoder = vgg_multi_layer_encoder(pretrained=False)
 
-    for arch, framework in zip(vgg_archs, frameworks):
-        with subtests.test(arch=arch, framework=framework):
-            if should_be_available(arch, framework):
-                assert isinstance(multi_layer_encoder.state_dict_url(framework), str)
-            else:
-                with pytest.raises(RuntimeError):
-                    multi_layer_encoder.state_dict_url(framework)
+        for arch, framework in zip(vgg_archs, frameworks):
+            with subtests.test(arch=arch, framework=framework):
+                if should_be_available(arch, framework):
+                    assert isinstance(
+                        multi_layer_encoder.state_dict_url(framework), str
+                    )
+                else:
+                    with pytest.raises(RuntimeError):
+                        multi_layer_encoder.state_dict_url(framework)
 
+    @pytest.mark.slow
+    def test_load_state_dict_smoke(self):
+        model = vgg(pretrained=False)
+        state_dict = model.state_dict()
 
-@pytest.mark.slow
-def test_vgg_multi_layer_encoder_load_state_dict_smoke():
-    model = vgg(pretrained=False)
-    state_dict = model.state_dict()
-
-    multi_layer_encoder = vgg_multi_layer_encoder()
-    multi_layer_encoder.load_state_dict(state_dict)
+        multi_layer_encoder = vgg_multi_layer_encoder()
+        multi_layer_encoder.load_state_dict(state_dict)
