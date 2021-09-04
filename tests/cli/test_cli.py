4987144a9e93eddda27afe9cea78a39f0fe6c99f
Philip Meier <github.pmeier@posteo.de>
2021-09-04 23:57:29 +0200

    add verbose option to CLI (#553)
    * add verbose option to CLI
    
    * add tests

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -92,9 +92,72 @@ def test_version(option, set_argv):
 
 @pytest.mark.slow
 def test_smoke(mock_image_optimization, mock_write_image, set_argv):
-    mock_image_optimization()
-    mock_write_image()
+    optim_mock = mock_image_optimization()
+    io_mock = mock_write_image()
     set_argv()
 
     with exits():
         main()
+
+    optim_mock.assert_called_once()
+    io_mock.assert_called_once()
+
+
+@pytest.mark.slow
+class TestVerbose:
+    @pytest.mark.parametrize("option", ["-v", "--verbose"])
+    def test_smoke(self, mock_image_optimization, mock_write_image, set_argv, option):
+        mock_image_optimization()
+        mock_write_image()
+        # If the output file is not specified,
+        # we would get output to STDOUT regardless of -v / --verbose
+        set_argv(option, "--output-image=foo.jpg")
+
+        with contextlib.redirect_stdout(io.StringIO()) as output, exits():
+            main()
+
+        assert output.getvalue().strip()
+
+    def test_device(self, mock_image_optimization, mock_write_image, set_argv):
+        device = "cpu"
+
+        mock_image_optimization()
+        mock_write_image()
+        set_argv("--verbose", f"--device={device}")
+
+        with contextlib.redirect_stdout(io.StringIO()) as output, exits():
+            main()
+
+        assert "'cpu'" in output.getvalue().strip()
+
+    def test_mle(self, mock_image_optimization, mock_write_image, set_argv):
+        mle = "vgg19"
+
+        mock_image_optimization()
+        mock_write_image()
+        set_argv("--verbose", f"--multi-layer-encoder={mle}")
+
+        with contextlib.redirect_stdout(io.StringIO()) as output, exits():
+            main()
+
+        output = output.getvalue().strip()
+        assert "VGGMultiLayerEncoder" in output
+        assert f"arch={mle}" in output
+
+    def test_perceptual_loss(self, mock_image_optimization, mock_write_image, set_argv):
+        content_loss = "FeatureReconstruction"
+        style_loss = "Gram"
+
+        mock_image_optimization()
+        mock_write_image()
+        set_argv(
+            "--verbose", f"--content-loss={content_loss}", f"--style-loss={style_loss}",
+        )
+
+        with contextlib.redirect_stdout(io.StringIO()) as output, exits():
+            main()
+
+        output = output.getvalue().strip()
+        assert "PerceptualLoss" in output
+        assert content_loss in output
+        assert style_loss in output
