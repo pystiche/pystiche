a26a3cc58a73dc6dcf0805e99b808c56b38d828e
Philip Meier <github.pmeier@posteo.de>
2021-09-04 23:13:51 +0200

    add smoke test for CLI (#554)

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -7,29 +7,67 @@ import pytest
 import pystiche
 from pystiche._cli import main
 
+from tests.mocks import make_mock_target
+
+
+@pytest.fixture
+def mock_image_optimization(mocker):
+    def mock():
+        return mocker.patch(make_mock_target("_cli", "write_image"))
+
+    return mock
+
+
+@pytest.fixture
+def mock_write_image(mocker):
+    def mock():
+        return mocker.patch(make_mock_target("_cli", "image_optimization"))
+
+    return mock
+
 
 @pytest.fixture
 def set_argv(mocker):
-    def patch_argv_(*args):
-        return mocker.patch.object(sys, "argv", ["pystiche", *args])
+    def set_argv_(*options, content_image="bird1", style_image="paint"):
+        return mocker.patch.object(
+            sys, "argv", ["pystiche", *options, content_image, style_image]
+        )
 
-    return patch_argv_
+    return set_argv_
 
 
 @contextlib.contextmanager
-def exits(*, code=None, error=False):
+def exits(*, should_succeed=True, expected_code=None):
     with pytest.raises(SystemExit) as info:
-        yield
+        with contextlib.redirect_stderr(io.StringIO()) as output:
+            yield
 
-    ret = info.value.code
+    returned_code = info.value.code or 0
+    succeeded = returned_code == 0
 
-    if code is not None:
-        assert ret == code
+    if expected_code is not None:
+        if returned_code == expected_code:
+            return
 
-    if error:
-        assert ret >= 1
+        raise AssertionError(
+            f"Returned and expected return code mismatch: "
+            f"{returned_code} != {expected_code}."
+        )
+
+    if should_succeed:
+        if succeeded:
+            return
+
+        raise AssertionError(
+            f"Program should have succeeded, "
+            f"but returned code {returned_code} and printed the following to STDERR: "
+            f"'{output.getvalue().strip()}'."
+        )
     else:
-        assert ret is None or ret == 0
+        if not succeeded:
+            return
+
+        raise AssertionError("Program shouldn't have succeeded, but did.")
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
@@ -50,3 +88,13 @@ def test_version(option, set_argv):
         main()
 
     assert output.getvalue().strip() == pystiche.__version__
+
+
+@pytest.mark.slow
+def test_smoke(mock_image_optimization, mock_write_image, set_argv):
+    mock_image_optimization()
+    mock_write_image()
+    set_argv()
+
+    with exits():
+        main()
